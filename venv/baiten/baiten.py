1ac9673c5c0bf745dcab2e72d35e3fb8262306c6
zong.xi <zong.xi@medsoftsys.com>
2020-01-06 15:12:50 +0800

    多平台测试

--- venv/baiten/baiten.py
+++ venv/baiten/baiten.py
@@ -2,27 +2,35 @@ from selenium import webdriver
 from pyvirtualdisplay import Display
 import sys, os
 
-sysPlatform = sys.platform.lower()    # 系统类型
-assert(sysPlatform == "linux")
+def run(driverPath):
+    driver = webdriver.Chrome(driverPath)
+    driver.close()
 
-try:
-    display = Display(visible=0, size=(800, 800))
-except Exception as e:
-    ErrorStr = str(e)
-    if "No such file or directory: 'Xvfb'" in ErrorStr:
-        import subprocess
-        subprocess.getoutput("sudo apt-get install xvfb")
-        subprocess.getoutput("pip install xvfbwrapper")
-display = Display(visible=0, size=(800, 800))
-display.start()
+def main():
+    binPath = os.path.join(os.path.dirname(os.path.dirname(__file__)), "bin")
+    driverPath = os.path.join(binPath, "chromedriver")
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-lastdir = os.path.dirname(os.path.dirname(__file__))
-sysPlatform = sys.platform.lower()
-if sysPlatform == "linux":
-    chrome_options.binary_location = lastdir + r"/bin/chromedriver"
-elif sysPlatform == "win32":
-    chrome_options.binary_location = lastdir + r"/bin/chromedriver.exe"
+    sysPlatform = sys.platform.lower()
+    if sysPlatform == "linux":
+        try:
+            display = Display(visible=0, size=(800, 800))
+        except Exception as e:
+            ErrorStr = str(e)
+            if "No such file or directory: 'Xvfb'" in ErrorStr:
+                import subprocess
+                subprocess.getoutput("sudo apt-get install xvfb")
+                subprocess.getoutput("pip install xvfbwrapper")
+        display = Display(visible=0, size=(800, 800))
+        display.start()
+    elif sysPlatform == "win32":
+        driverPath += ".exe"
 
-driver = webdriver.Chrome(options=chrome_options)
+    if os.path.exists(driverPath):
+        return False
+
+    sys.path.append(binPath)
+    run(driverPath)
+    return True
+
+if __name__ == "__main__":
+    main()
