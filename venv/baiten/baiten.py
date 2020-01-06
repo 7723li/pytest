bec9eeb8bfd51eadc8f6116777450b99d624bde4
zong.xi <zong.xi@medsoft.com>
2020-01-06 11:43:15 +0800

    chrome driver env

--- venv/baiten/baiten.py
+++ venv/baiten/baiten.py
@@ -1,5 +1,12 @@
 from selenium import webdriver
+import sys
 
 option = webdriver.ChromeOptions()
-option.binary_location = r"/home/lzx/Desktop/testpy/venv/bin/chromedriver"
+
+sysPlatform = sys.platform  # 系统类型
+if sysPlatform == "linux":
+    option.binary_location = r"/home/lzx/Desktop/testpy/venv/bin/chromedriver"
+elif sysPlatform == "win32":
+    None
+
 driver = webdriver.Chrome(options=option)
