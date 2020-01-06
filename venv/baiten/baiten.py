de4605d282e607520cb353f357a0f7ad10b5c180
zong.xi <zong.xi@medsoft.com>
2020-01-06 14:34:29 +0800

    多平台测试

--- venv/baiten/baiten.py
+++ venv/baiten/baiten.py
@@ -1,12 +1,28 @@
 from selenium import webdriver
-import sys
+from pyvirtualdisplay import Display
+import sys, os
 
-option = webdriver.ChromeOptions()
+sysPlatform = sys.platform.lower()    # 系统类型
+assert(sysPlatform == "linux")
 
-sysPlatform = sys.platform  # 系统类型
+try:
+    display = Display(visible=0, size=(800, 800))
+except Exception as e:
+    ErrorStr = str(e)
+    if "No such file or directory: 'Xvfb'" in ErrorStr:
+        import subprocess
+        subprocess.getoutput("sudo apt-get install xvfb")
+        subprocess.getoutput("pip install xvfbwrapper")
+display = Display(visible=0, size=(800, 800))
+display.start()
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+lastdir = os.path.dirname(os.path.dirname(__file__))
+sysPlatform = sys.platform.lower()
 if sysPlatform == "linux":
-    option.binary_location = r"/home/lzx/Desktop/testpy/venv/bin/chromedriver"
+    chrome_options.binary_location = lastdir + r"/bin/chromedriver"
 elif sysPlatform == "win32":
-    None
+    chrome_options.binary_location = lastdir + r"/bin/chromedriver.exe"
 
-driver = webdriver.Chrome(options=option)
+driver = webdriver.Chrome(options=chrome_options)
