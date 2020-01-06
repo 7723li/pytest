798909bf209389eaa23c7a3276ae50643a07a39d
zong.xi <zong.xi@medsoft.com>
2020-01-06 11:39:30 +0800

    chrome driver env

--- venv/baiten/baiten.py
+++ venv/baiten/baiten.py
@@ -1,1 +1,5 @@
 from selenium import webdriver
+
+option = webdriver.ChromeOptions()
+option.binary_location = r"/home/lzx/Desktop/testpy/venv/bin/chromedriver"
+driver = webdriver.Chrome(options=option)
