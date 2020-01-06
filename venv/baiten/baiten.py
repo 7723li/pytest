bac629cfaa069a64188c953a582fc45801ce755e
zong.xi <zong.xi@medsoft.com>
2020-01-06 16:02:00 +0800

    change to fire fox

--- venv/baiten/baiten.py
+++ venv/baiten/baiten.py
@@ -3,32 +3,22 @@ from pyvirtualdisplay import Display
 import sys, os
 
 def run(driverPath):
-    driver = webdriver.Chrome(driverPath)
+    driver = webdriver.Firefox()
     driver.close()
 
 def main():
     binPath = os.path.join(os.path.dirname(os.path.dirname(__file__)), "bin")
-    driverPath = os.path.join(binPath, "chromedriver")
-
+    sys.path.append(binPath)
+    
     sysPlatform = sys.platform.lower()
     if sysPlatform == "linux":
-        try:
-            display = Display(visible=0, size=(800, 800))
-        except Exception as e:
-            ErrorStr = str(e)
-            if "No such file or directory: 'Xvfb'" in ErrorStr:
-                import subprocess
-                subprocess.getoutput("sudo apt-get install xvfb")
-                subprocess.getoutput("pip install xvfbwrapper")
-        display = Display(visible=0, size=(800, 800))
-        display.start()
+        driverPath = os.path.join(binPath, "chromedriver")
     elif sysPlatform == "win32":
-        driverPath += ".exe"
+        driverPath = os.path.join(binPath, "chromedriver.exe")
 
     if not os.path.exists(driverPath):
         return False
-
-    sys.path.append(binPath)
+    
     run(driverPath)
     return True
 
