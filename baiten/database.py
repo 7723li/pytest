e8d1e381a7d74c83c415f4e14a0e86ec708749a1
lzx <542131220@qq.com>
2020-01-07 23:59:52 +0800

    almost finish

--- baiten/database.py
+++ baiten/database.py
@@ -1,18 +1,58 @@
 import pymysql
 
-class baidten_db_struct:
-    apply_number = str()
-    apply_date = str()
-    public_number = str()
-    public_data = str()
-    apply_member = str()
-    invent_member = str()
+test_data = {
+    '申请号': 'CN201420604639.6', 
+    '申请日': '20141020', 
+    '公开号': 'CN204072058U', 
+    '授权公告日': '20150107', 
+    '申请（专利权）人': '广州医软智能科技有限公司', 
+    '发明人': '刘发杰 郑鹏飞 亚历克斯·布兰多 罗晓川', 
+    '专利类型': '中国实用新型', 
+    '专利名': '多层次微循环状态监测装置', 
+    '法律状态': '有权-审定授权'
+    }
 
 class baidten_db:
     def __init__(self):
-        self.connection = pymysql.connect(host='localhost',
-                                            port=3306,
-                                            user='root',
-                                            password='tc0301',
-                                            charset='utf8')
+        self.connection = \
+            pymysql.connect(host='localhost',
+                            port=3306,
+                            user='root',
+                            password='tc0301',
+                            database='baiten_db',
+                            charset='utf8')
         self.cursor = self.connection.cursor()
+
+        # 手动查询表格列表
+        self.cursor.execute("show tables;")
+        table_list = [tuple[0] for tuple in self.cursor.fetchall()]
+        if "medsoft_baiten_db" in table_list:
+            return
+
+        create_table_sql =  "create table if not exists medsoft_baiten_db ("   # 不存在才创建
+        create_table_sql += "`id` INT UNSIGNED AUTO_INCREMENT primary key,"    # 主键
+        create_table_sql += "apply_num varchar(32),"                           # 申请号
+        create_table_sql += "apply_date varchar(8),"                           # 申请日
+        create_table_sql += "public_num varchar(32),"                          # 公开号
+        create_table_sql += "public_date varchar(8),"                          # 公开日
+        create_table_sql += "aplly_member varchar(128),"                       # 申请（专利权）人
+        create_table_sql += "invent_member varchar(512),"                      # 发明人
+        create_table_sql += "patent_type varchar(16),"                         # 专利类型
+        create_table_sql += "patent_name varchar(64),"                         # 专利名称
+        create_table_sql += "law_status varchar(32)"                           # 法律状态
+        create_table_sql += ") character set utf8;"
+        self.cursor.execute(create_table_sql)
+        
+    def insert_one(self, src_data):
+        assert(type(src_data) is dict)
+
+        data = list(src_data.values())
+        assert(len(data) == 9)
+        
+        insert_sql = "insert into medsoft_baiten_db \
+        (apply_num, apply_date, public_num, public_date, aplly_member, invent_member, patent_type, patent_name, law_status) \
+        values  \
+        (%s, %s, %s, %s, %s, %s, %s, %s, %s);"
+
+        self.cursor.execute(insert_sql, data)
+        self.connection.commit()        # 数据库事务
--- baiten/main.py
+++ baiten/main.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 from urllib import parse
 import sys, os, logging, time, random
+import database
 
 # 日志输出
 logging.basicConfig(filename='baiten.log', 
@@ -15,11 +17,62 @@ sys_platform = sys.platform.lower()
 logging.info("\n")
 logging.info("platform : " + sys_platform)
 
+# 针对佰腾应用的数据库
+baidten_db = database.baidten_db()
+
+'''
+shit fucking happends
+有时候页面未加载完 会造成xpath取节点时崩溃
+'''
+def avoid_being_fuck_by_selenium_xpath(browser_driver, xpath_code):
+    max_retry_times = 20
+    load_sunccess = False
+    res = None
+
+    while False == load_sunccess and max_retry_times > 0:
+        try:
+            res = browser_driver.find_elements_by_xpath(xpath_code)
+            load_sunccess = True
+        except NoSuchElementException:
+            time.sleep(1)
+            max_retry_times -= 1
+
+    if res is None:
+        logging.debug("fuck by selenium")
+
+    assert(res is not None)
+    return res
+
+def kill_process(prog_name):
+    cmd = "ps -ef | grep \"" + prog_name + "\" | grep -v grep | awk '{print $2}'"
+    find_res = os.popen(cmd).read().split('\n')
+
+    logging.info("find %d %s running", len(find_res), prog_name)
+
+    kill = 0
+    for pid in find_res:
+        os.popen("kill " + pid).read()
+        kill += 1
+
+    logging.info("kill %d %s", kill, prog_name)
+
 '''
 从每个专利的专属链接提取数据
 '''
 def get_data_from_url(browser_driver, url):
-    browser_driver.get(url)
+    get_url_success = False
+    retry_times = 0
+    while not get_url_success and retry_times < 5:
+        try:
+            browser_driver.get(url)
+            get_url_success = True
+        except NoSuchElementException:
+            try:
+                is_banded_xpath = avoid_being_fuck_by_selenium_xpath(browser_driver, '//div[@class="payValidate"]')[0]
+                logging.debug("be baned : " + is_banded_xpath.text)
+            except:
+                retry_times += 1
+
     time.sleep(random.randrange(1,5))
 
     # 打表 字段对应数据
@@ -32,7 +85,7 @@ def get_data_from_url(browser_driver, url):
     main_matter_label2data["发明人"] = str()    
 
     # 主要事项 及其 描述字段
-    main_matters_labels_xpath = browser_driver.find_elements_by_xpath('//ul[@class="abst-info fn-clear"]/li')
+    main_matters_labels_xpath = avoid_being_fuck_by_selenium_xpath(browser_driver, '//ul[@class="abst-info fn-clear"]/li')
     for i in range(len(main_matters_labels_xpath)):
         key_value = main_matters_labels_xpath[i].text.split('\n')
         label_key = key_value[0]
@@ -41,16 +94,15 @@ def get_data_from_url(browser_driver, url):
         
         main_matter_label2data[label_key] = key_value[1]
     
-    patentType_pantentName_xpath = browser_driver.find_element_by_xpath('//span[@class="title Js_hl"]')
+    patentType_pantentName_xpath = avoid_being_fuck_by_selenium_xpath(browser_driver, '@class="title Js_hl"')[0]
     patentType_pantentName = patentType_pantentName_xpath.text.strip().split(" ")
 
     main_matter_label2data["专利类型"] = patentType_pantentName[0].replace('[', '').replace(']', '')
     main_matter_label2data["专利名"] = patentType_pantentName[-1]
-    main_matter_label2data["法律状态"] = browser_driver.find_element_by_xpath('//div[@class="law-status law-status2"]/p').text
+    main_matter_label2data["法律状态"]  = avoid_being_fuck_by_selenium_xpath(browser_driver, '//div[@class="law-status law-status2"]/p')[0].text
 
     print(url)
-    with open("debug.txt", "ab+") as debugtxt:
-        debugtxt.write((str(main_matter_label2data) + "\n").encode("utf-8"))
+    baidten_db.insert_one(main_matter_label2data)
 
 '''
 获取每个专利的专属链接
@@ -59,10 +111,13 @@ def get_child_urls(browser_driver, go_url):
     child_urls = []
 
     logging.info("processing url %s", go_url)
-    browser_driver.get(go_url)
+    for i in range(3):
+        browser_driver.get(go_url)
+        time.sleep(1)
+
     time.sleep(random.randrange(1,5))   # 稍作停顿
 
-    public_id_list = browser_driver.find_elements_by_xpath('//a[contains(@title, "公开号") and @class="c-blue"]')
+    public_id_list = avoid_being_fuck_by_selenium_xpath(browser_driver, '//a[contains(@title, "公开号") and @class="c-blue"]')
     for public_id in public_id_list:
         child_urls.append(public_id.get_attribute("href"))
     
@@ -89,12 +144,12 @@ def start_browser(browser_driver):
     child_url_set.extend(child_urls)
 
     # 总共含有的页数
-    all_page_num_xpath = browser_driver.find_element_by_xpath('//span[@class="btui-paging-totalPage"]/em')
+    all_page_num_xpath = avoid_being_fuck_by_selenium_xpath(browser_driver, '//span[@class="btui-paging-totalPage"]/em')[0]
     all_page_num = int(all_page_num_xpath.text.strip())
     logging.info("totally contains %d pages", all_page_num)
 
     # go to collect from page 2 until end
-    for page in range(2, all_page_num + 1):
+    for page in (2, all_page_num + 1):
         child_urls = get_child_urls(browser_driver, origin_url + str(single_page_obj_num) + "/" + str(page))
         child_url_set.extend(child_urls)
 
@@ -193,7 +248,8 @@ def main():
     return True
 
 if __name__ == "__main__":
+    kill_process("firefox")
+
     main()
-    if sys_platform == "linux":
-        killff = os.popen("kill -9 $(pidof firefox)").read()
-        logging.debug("kill firefox " + killff)
+
+    kill_process("firefox")
